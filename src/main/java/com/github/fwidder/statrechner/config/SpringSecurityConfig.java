329edc4beb37b23ef978a9cb76e4811ac050eec5
Florian Widder (HV11133) <florian.widder@devk.de>
2020-03-06 17:29:43 +0100

    Add Spring Security

---
+++ src/main/java/com/github/fwidder/statrechner/config/SpringSecurityConfig.java
@@ -0,0 +1,43 @@
+package com.github.fwidder.statrechner.config;
+
+import com.github.fwidder.statrechner.service.UserDetailsServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    UserDetailsServiceImpl userDetailsService;
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+
+        http
+                //HTTP Basic authentication
+                .httpBasic()
+                .and()
+                .authorizeRequests()
+                .antMatchers("/rest/**", "/api/**","/web/**", "/swagger**")
+                .authenticated()
+                .and()
+                .csrf().disable()
+                .formLogin();
+    }
+}
